628a3025473540ba108d201a2548eb9afef9ba55
nullbr <bmarianoleite3@gmail.com>
2022-03-10 15:50:24 -0300

    "Copied app files from orbitapp"

---
+++ app/controllers/html_pages_controller.rb
@@ -0,0 +1,14 @@
+class HtmlPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+  end
+  
+end
---
+++ test/controllers/html_pages_controller_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class HtmlPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get root_path
+    assert_response :success
+    assert_select "title", "Home | Orbit Feedback Application"
+  end
+
+  test "should get help" do
+    get help_path
+    assert_response :success
+    assert_select "title", "Help | Orbit Feedback Application"
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "About | Orbit Feedback Application"
+  end
+
+  test "should get contact page" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Orbit Feedback Application"
+  end
+end
