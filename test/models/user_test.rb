cec8d8f8516761b13507d67b89d33ecd8cf7498a
nullbr <bmarianoleite3@gmail.com>
2022-03-11 10:18:13 -0300

    small changes to files

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Test User", email: "test@example.com" )
+  end
+  
+  test "should be valid" do
+    assert @user.valid
+  end
 end
